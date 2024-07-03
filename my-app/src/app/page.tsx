b63d54d0076c235f06797fc3f6a3e68f3019a7cb
sneha404 <snehajaisinghani3005@gmail.com>
2024-07-03 07:57:16 +0530

    Initial commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import React from 'react'
+
+
+export default function page() {
+  return (
+    <>
+
+      <div className="sec1">
+        <div className="r1">
+          <div className="c1">
+            Sol
+            <span>ution</span>
+          </div>
+          <div className="c2">
+            <span>Home</span>
+            <span>Sevice</span>
+            <span>About</span>
+            <span>Team</span>
+            <span>Pricing</span>
+            <span>Portfolio</span>
+            <span>Contact</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="sec2">
+        <div className="r2">
+          <div className='r2a'>
+            <div>SOLUTION COMPANY</div>
+            <div>WE DESIGN & CODE WEBSITE</div>
+            <div>OUR WORK</div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
